6dc7ad77241310e20b24a31b98f6ddd0eeecc523
GeraldineSaco <gsacoruiz@gmail.com>
2026-03-22 00:17:37 +0100

    feat: Procedure entity done

---
+++ src/main/java/com/projectsingle/migracheck/entity/Procedure.java
@@ -0,0 +1,16 @@
+package com.projectsingle.migracheck.entity;
+
+public enum Procedure {
+    BANCOS,
+    VIVIENDA,
+    EMPADRONARSE,
+    TURISMO_GAUDIRMES,
+    LINEA_MOVIL,
+    TARJETA_SANITARIA,
+    IDCAT_MOVIL,
+    TARJETA_TRANSPORTE,
+    CARNÉ_BIBLIOTECA,
+    ESCOLARIZACIÓN,
+    HOMOLOGACIÓN_ESTUDIOS,
+    INTEGRACIÓN
+}
