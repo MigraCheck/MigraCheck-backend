d07c5d515413ff5352957f7446fbc76169a66f24
GeraldineSaco <gsacoruiz@gmail.com>
2026-03-21 19:22:44 +0100

    feature: instalation spring boot in project

---
+++ migracheck/src/main/java/com/projectsingle/migracheck/MigracheckApplication.java
@@ -0,0 +1,13 @@
+package com.projectsingle.migracheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigracheckApplication.class, args);
+	}
+
+}
---
+++ migracheck/src/test/java/com/projectsingle/migracheck/MigracheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsingle.migracheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigracheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
