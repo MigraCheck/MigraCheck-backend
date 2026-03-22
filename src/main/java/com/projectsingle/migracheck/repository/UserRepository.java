5247ab2b0332289fb48e73da5eae41be97482ed0
GeraldineSaco <gsacoruiz@gmail.com>
2026-03-22 09:55:33 +0100

    feat: UserRepository done

---
+++ src/main/java/com/projectsingle/migracheck/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projectsingle.migracheck.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projectsingle.migracheck.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    
+}
